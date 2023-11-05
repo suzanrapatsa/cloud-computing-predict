5f9eedaed2d04a4f8085c5275de788113e850277
Kgoadiba Suzan Rapatsha <141473654+suzanrapatsa@users.noreply.github.com>
2023-11-05 09:28:37 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url:"https://lvewek6sx1.execute-api.eu-west-1.amazonaws.com/default/kgoadiba_rapatsha-Lambda-Function",
+                url:"https://ahxowxlnv7.execute-api.eu-west-1.amazonaws.com/default/Suzan_Function",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
